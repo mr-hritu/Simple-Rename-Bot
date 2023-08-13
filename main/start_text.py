cdb0e013d33187b42e82ddf6115cf2187e5b7f38
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-08-13 20:06:04 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -2,6 +2,20 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
 from config import ADMIN
  
+@Client.on_message(filters.text & filters.private & filters.incoming)
+async def fore(c, m):
+      try:
+        chat = await c.get_chat_member(-1001785446911, m.from_user.id)
+        if chat.status=="kicked":
+           await c.send_message(chat_id=m.chat.id, text="You are Banned ☹️\n\n📝 If u think this is an ERROR message in @Privates_Chats", reply_to_message_id=m.id)
+           m.stop_propagation()
+      except UserBannedInChannel:
+         return await c.send_message(chat_id=m.chat.id, text="Hai you made a mistake so you are banned from channel so you are banned from me too 😜")
+      except UserNotParticipant:
+          button = [[InlineKeyboardButton('Updates Channel 🇮🇳', url='https://t.me/Private_Bots')]]
+          markup = InlineKeyboardMarkup(button)
+          return await c.send_message(chat_id=m.chat.id, text="""Hai bro,\n\nYou must join my channel for using me.\n\nPress this button to join now 👇""", reply_markup=markup)
+      m.continue_propagation()
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
